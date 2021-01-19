61c37bb63d62791dee8ff54aff97b8ec70f48c2d
Lily Meng <LilyxMeng@github.com>
2021-01-18 23:18:52 -0500

    updated submit button js function

--- js/contact.js
+++ js/contact.js
@@ -4,10 +4,10 @@ function finishedForm() {
     var email = document.getElementById("email-input").value;
     var message = document.getElementById("message-input").value;
 
-    if (fname.length > 0 && lname.length > 0 && email.length > 0 && message.length > 0) {
+    if (fname.length > 0 && lname.length > 0 && email.length > 0 && message.length > 0 && email.includes("@")  && (email.endsWith(".com") || (email.endsWith(".ca")))) {
         document.getElementById("finished-form").innerHTML = "Thank you " + fname + " for submitting the form! We will get back to you as soon as possible!";
     } else {
-        document.getElementById("finished-form").innerHTML = "Please make sure all boxes are filled out.";
+        document.getElementById("finished-form").innerHTML = "Please make sure all boxes are filled out with the correct information.";
     }
 }
 
