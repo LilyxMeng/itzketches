332fc413c9b5e78f47c57b01e74b500e33c91349
Lily Meng <LilyxMeng@github.com>
2021-01-20 18:26:23 -0500

    updated styling

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,14 @@
 function finishedForm() {
-    var fname = document.getElementById("fname-input").value;
-    var lname = document.getElementById("lname-input").value;
+    var name = document.getElementById("name-input").value;
     var email = document.getElementById("email-input").value;
     var message = document.getElementById("message-input").value;
 
-    if (fname.length > 0 && lname.length > 0 && email.length > 0 && message.length > 0 && email.includes("@")  && (email.endsWith(".com") || (email.endsWith(".ca")))) {
-        document.getElementById("finished-form").innerHTML = "Thank you " + fname + " for submitting the form! We will get back to you as soon as possible!";
+    if (name.length > 0 && email.length > 0 && message.length > 0) {
+        document.getElementById("finished-form").innerHTML = "Thank you for submitting the form! We will get back to you as soon as possible!";
     } else {
         document.getElementById("finished-form").innerHTML = "Please make sure all boxes are filled out with the correct information.";
     }
 }
 
+//statement incomplete for email validation
 //reset boxes so theyre empty after user fills form
